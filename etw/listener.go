4cd42690c4ad2b3e93cd0c2757f4d066da355ffa
Mariana <mariana@elastic.co>
2020-04-02 15:53:43 +0200

    work on etw

--- etw/listener.go
+++ etw/listener.go
@@ -1,1 +1,63 @@
  package etw
+
+ import (
+	 "bufio"
+	 "fmt"
+	 "github.com/Microsoft/go-winio/pkg/guid"
+	 "os"
+
+	 "github.com/Microsoft/go-winio/pkg/etw"
+	 "github.com/sirupsen/logrus"
+ )
+
+ func callback(sourceID guid.GUID, state etw.ProviderState, level etw.Level, matchAnyKeyword uint64, matchAllKeyword uint64, filterData uintptr) {
+	_= sourceID
+	_= matchAllKeyword
+	_= filterData
+	 fmt.Printf("Callback: isEnabled=%d, level=%d, matchAnyKeyword=%d\n", state, level, matchAnyKeyword)
+ }
+
+ func test() {
+	 provider, err := etw.NewProvider("TestProvider", callback)
+
+	 if err != nil {
+		 logrus.Error(err)
+		 return
+	 }
+	 defer func() {
+		 if err := provider.Close(); err != nil {
+			 logrus.Error(err)
+		 }
+	 }()
+
+	 fmt.Printf("Provider ID: %s\n", provider)
+
+	 reader := bufio.NewReader(os.Stdin)
+
+	 fmt.Println("Press enter to log events")
+	 reader.ReadString('\n')
+
+	 if err := provider.WriteEvent(
+		 "TestEvent",
+		 etw.WithEventOpts(
+			 etw.WithLevel(etw.LevelInfo),
+			 etw.WithKeyword(0x140),
+		 ),
+		 etw.WithFields(
+			 etw.StringField("TestField", "Foo"),
+			 etw.StringField("TestField2", "Bar"),
+			 etw.Struct("TestStruct",
+				 etw.StringField("Field1", "Value1"),
+				 etw.StringField("Field2", "Value2")),
+			 etw.StringArray("TestArray", []string{
+				 "Item1",
+				 "Item2",
+				 "Item3",
+				 "Item4",
+				 "Item5",
+			 })),
+	 ); err != nil {
+		 logrus.Error(err)
+		 return
+	 }
+ }
